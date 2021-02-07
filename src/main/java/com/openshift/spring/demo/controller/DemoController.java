9228409e767db1a197f63401bbf04d0b99f73403
"Pavel Simonovsky" <"Pavel Simonovsky"@L608217.corp.teranet.ca>
2021-02-06 23:57:01 -0500

    test

---
+++ src/main/java/com/openshift/spring/demo/controller/DemoController.java
@@ -0,0 +1,32 @@
+/**
+ * <pre>
+ * Copyright 2021
+ * Teranet Inc.,
+ * 123 Front Street West, Suite 700,
+ * Toronto, Ontario,
+ * CANADA M5J 2M2
+ *
+ * All rights reserved. This software is the confidential and proprietary information
+ * of Teranet,Inc. ('Confidential Information'). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the license agreement you
+ * entered into with Teranet.
+ * </pre>
+ */ 
+package com.openshift.spring.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Pavel Simonovsky
+ *
+ */
+@RestController
+public class DemoController {
+
+  @GetMapping("hello")
+  public String sayHello(@RequestParam String name) {
+    return "Hello " + name;
+  }
+}
